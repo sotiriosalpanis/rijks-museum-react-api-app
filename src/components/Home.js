f9104244a81c39038465ae9d9ae5539d2101cd90
Jacqueline de Leeuw <jacqueline.de.leeuw@gmail.com>
2021-03-12 11:27:45 +0000

    carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,36 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Carousel from 'react-multi-carousel'
 
 const Home = () => {
-
-  const responsive = {
-    superLargeDesktop: {
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5,
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-    },
-  }
-
   return (
-    <section className="hero is-fullheight-with-navbar is-info">
-      <Carousel responsive={responsive}>
-        <div>Item 1</div>
-        <div>Item 2</div>
-        <div>Item 3</div>
-        <div>Item 4</div>
-      </Carousel>
+    <section className="hero is-fullheight-with-navbar" 
+      style= {{
+        backgroundImage: 'url()',
+      }}>      
       <div className="home-content">
         <div className="hero-body">
           <p className="title is-1">
