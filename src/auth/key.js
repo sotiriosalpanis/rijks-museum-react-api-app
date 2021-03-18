d20a24b8a2b5abcc7e2d164df8bee88f5419d891
salpanis <salpanis@gmail.com>
2021-03-18 14:56:35 +0000

    updated with .env

--- src/auth/key.js
+++ src/auth/key.js
@@ -1,6 +1,7 @@
 export const getAccessKey = () => {
-  return 'CIMyuFiL'
+  return process.env.REACT_APP_API_KEY
 }
+
 
 export const languageSelected = () => {
   return window.localStorage.getItem('language')
